68a25f5553c3083b4ec5c1bbabf3980f560bbb13
minhdq99hp <minhdq99hp@gmail.com>
2020-11-22 12:16:28 +0700

    update

---
+++ python/utilities/image.py
@@ -0,0 +1,32 @@
+'''
+Utility functions for working with images.
+
+Author: minhdq99hp@gmail.com
+'''
+from PIL import Image
+from os.path import splitext
+
+class UnsupportedExtension(Extension):
+    pass
+
+def convert_image(input_path, output_path, o_ext=None):
+    '''
+    Use PIL.Image to convert image format.
+    The output extension will be determined from output_path. 
+    You can also specify it by using o_ext.
+
+    Supported extensions: png, jpeg, webp
+    Warning: 
+    - Every image will be convert to RGB, 
+      which mean the output image will not have alpha value.
+    '''
+    supported_exts = ('png', 'jpeg', 'webp')
+    if not o_ext:
+        _, ext = splitext(output_path)
+        o_ext = ext[1:].lower().replace('jpg', 'jpeg')
+
+    if o_ext not in supported_exts:
+        raise UnsupportedExtension(f"Can't convert image to {o_ext}")
+
+    im = Image.open(input_path).convert("RGB")
+    im.save(output_path, o_ext)
--- python/utilities/request.py
+++ python/utilities/request.py
@@ -9,20 +9,35 @@ from time import sleep
 class RetryLimitExceededException(Exception):
     pass
 
-def send_json_request(url, data={}, timeout=60, max_retry=3, delay=0, silent=True):
-    '''Send a request with json data to url'''
-    
+def send_request(url, data={}, method='post', headers={}, timeout=60, max_retry=3, delay=0, silent=True, exc_msg=''):
+    '''
+    Send HTTP request to url with retry and delay if needed.
+
+    The sending data should be json. Otherwise, you have to specific headers to send request. 
+
+    If not silent:
+        RetryLimitExceededException will be raised after max_retry retries.
+    else:
+        return None
+    '''
+
     retry = 0
     while retry < max_retry:
         try:
-            res = requests.post(url, json=data, timeout=timeout)
+            if isinstance(data, dict):
+                res = requests.request(method, url, json=data, timeout=timeout, headers=headers)
+            elif data is not None:
+                res = requests.request(method, url, data=data, timeout=timeout, headers=headers)
+            else:
+                res = requests.request(method, url, timeout=timeout, headers=headers)
             return res
         except requests.exceptions.RequestException:
             retry += 1
             sleep(delay)
 
     if not silent:
-        raise RetryLimitExceededException(f"Unable to send json request after {max_retry} retries.")
+        raise RetryLimitExceededException(f"Unable to send request after {max_retry} retries.")
 
     return None
+
 
