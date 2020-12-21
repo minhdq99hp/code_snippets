de3d8c907b472d05f0959c6d959957bba91235f6
minhdq99hp <minhdq99hp@gmail.com>
2020-12-21 11:51:16 +0700

    add string.py

---
+++ python/utilities/string.py
@@ -0,0 +1,23 @@
+'''
+String utilities
+
+References:
+- Ants Aasma - [Answer in StackOverflow](https://stackoverflow.com/questions/92438/stripping-non-printable-characters-from-a-string-in-python)
+'''
+
+import unicodedata, re, itertools, sys
+
+all_chars = (chr(i) for i in range(sys.maxunicode))
+categories = {'Cc'}
+control_chars = ''.join(c for c in all_chars if unicodedata.category(c) in categories)
+# or equivalently and much more efficiently
+control_chars = ''.join(map(chr, itertools.chain(range(0x00,0x20), range(0x7f,0xa0))))
+
+control_char_re = re.compile('[%s]' % re.escape(control_chars))
+
+def remove_control_chars(s):
+    return control_char_re.sub('', s)
+
+
+def remove_duplicated_space(s):
+    return re.sub(r'\s+', ' ', s)
