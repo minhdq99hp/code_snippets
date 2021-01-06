dc1fb738f79186e1c0aec82474e45f8125e11c53
minhdq99hp <minhdq99hp@gmail.com>
2021-01-06 11:07:24 +0700

    update image.py

--- python/utilities/image.py
+++ python/utilities/image.py
@@ -3,10 +3,14 @@ Utility functions for working with images.
 Contributors: 
     - minhdq99hp@gmail.com
 '''
+import io
+import requests
+import traceback
+
 from PIL import Image
 from os.path import splitext
 
-class UnsupportedExtension(Extension):
+class UnsupportedExtension(Exception):
     pass
 
 def convert_image(input_path, output_path, o_ext=None):
@@ -30,3 +34,40 @@ def convert_image(input_path, output_path, o_ext=None):
 
     im = Image.open(input_path).convert("RGB")
     im.save(output_path, o_ext)
+
+
+def download_image_from_url(url):
+    """
+    Return a dictionary contains data of the image
+
+    Args:
+        url (str): direct link of the image
+
+    Returns:
+        dict: result data of the image
+
+            {
+                'status': 0 if success, others if error occurs.
+                'extension': the format of the image. Ex: png, jpeg,...
+                'file': binary data of the image
+            }
+    """
+    result = {'status': 1}
+    try:
+        res = requests.get(url, timeout=15, verify=False)
+    except requests.exceptions.RequestException:
+        traceback.print_exc()
+        return result
+    try:
+        data = res.content
+        im = Image.open(io.BytesIO(data))
+        im.verify()
+
+        result['extension'] = im.format.lower()
+        result['file'] = data
+    except Exception as e:
+        traceback.print_exc()
+        return result
+
+    result['status'] = 0
+    return result
