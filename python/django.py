2face4d001935fd3dbce66a35c8e7c1a6fbbbf7e
minhdq99hp <minhdq99hp@gmail.com>
2021-06-13 16:46:24 +0700

    update

--- python/django.py
+++ python/django.py
@@ -6,5 +6,11 @@ class HomeAPIView(APIView):
     def get(self, request, pk, format=None):
         task = get_object_or_404(Task, id=pk)
 
+        # get data from another view
+        resonse = SomeAPIView.as_view()(request=request._request).data
+
         return FileResponse(open(task.file.path, 'rb'))
+
+
+
 
